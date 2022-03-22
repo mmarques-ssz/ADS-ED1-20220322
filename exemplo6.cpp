5b5e51c7b532b4f6753a757ec7c9a96a93d80d56
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2022-03-22 20:46:33 -0300

    Add files via upload

---
+++ exemplo6.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int *v;
+	int *aux;
+	v = new int[10];
+	aux = v;
+	
+	// Carregando os valores no array
+	for(int i=0; i<10; ++i)
+	{
+		v[i] = i+1;
+	}
+	
+	// Exibindo os valores do array - percorrendo através do índice
+	for(int i=0; i<10; ++i)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+	
+	// Exibindo os valores do array - percorrendo através do endereço
+	for(int i=0; i<10; ++i)
+	{
+		cout << *v << " ";
+		++v;
+	}
+	cout << endl;
+	
+	// Reposicionamento do ponteiro no início do array	
+	v = aux;
+	
+	// Exibindo os valores do array - percorrendo através do índice
+	for(int i=0; i<10; ++i)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+	
+	// Exibindo os valores do array - percorrendo através do endereço
+	for(int i=0; i<10; ++i)
+	{
+		cout << *v << " ";
+		++v;
+	}
+	cout << endl;
+	
+	delete[] v;
+	
+	return 0;
+}
